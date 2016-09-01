1d773f9d6c62a7bd5dff92bd7bd531471f5646ce
henryzale <zaleh16@students.ecu.edu>
2016-09-01 11:36:37 -0400

    add assignment2

---
+++ assignment2 solutions/assignment2.m
@@ -0,0 +1,20 @@
+% Henry Zale
+% Due September 15th
+% Assignment 2 Solutions
+
+%% Chapter 1 Exercises 1-6, 11-15, 22, 24-27
+
+% 1.)
+
+
+
+
+%% Chapter 2 Exercises 1-4, 6-10, 12, 23, 26, 30, 31
+
+% 1.)
+
+
+
+%% Chapter 3 Exercises 1, 4, 6, 8, 13
+
+% 1.)
